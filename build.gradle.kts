6ed7863770a465c32f20be7807b2f65ae09e7c2b
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-10 09:04:44 -0400

    chore: initialize root gradle project with wrapper

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+}
+
+allprojects {
+    group = "com.switchboard"
+    version = "0.1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
