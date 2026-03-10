21562da570ba165fc51853ddce271bfdd88fb2c5
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-10 09:56:51 -0400

    feat: add condition value object and op enum

---
+++ switchboard-server/src/main/java/com/switchboard/domain/model/Condition.java
@@ -0,0 +1,5 @@
+package com.switchboard.domain.model;
+
+public record Condition(String attribute, Operator operator, String value) {
+
+}
---
+++ switchboard-server/src/main/java/com/switchboard/domain/model/Operator.java
@@ -0,0 +1,14 @@
+package com.switchboard.domain.model;
+
+public enum Operator {
+    EQUALS,
+    NOT_EQUALS,
+    CONTAINS,
+    STARTS_WITH,
+    IN_LIST,
+    NOT_IN_LIST,
+    GREATER_THAN,
+    LESS_THAN,
+    SEMVER_GT,
+    SEMVER_LT
+}
