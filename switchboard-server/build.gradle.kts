e5cd395bf00aa426b06ded4ab3f8ecfec1db681d
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-03-19 09:29:56 -0400

    chore: add flyway and jpa dependencies with database config

--- switchboard-server/build.gradle.kts
+++ switchboard-server/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
